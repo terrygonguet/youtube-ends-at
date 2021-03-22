de5e7ff8c512def896af0ed638204f637015842f
terrygonguet <terry@gonguet.com>
2021-03-22 06:13:16 +0100

    Better looking section

--- scripts/content.js
+++ scripts/content.js
@@ -2,22 +2,39 @@ setInterval(function () {
 	const player = document.querySelector("video")
 	if (!player) return
 
-	const timeDisplay = document.querySelector(".ytp-time-display")
-	if (!timeDisplay) return
+	const container = document.querySelector("#endsAtContainer")
+	if (!container) {
+		const timeDisplay = document.querySelector(".ytp-time-display")
+		if (!timeDisplay) return
 
-	const finishesAtLabel = timeDisplay.querySelector("#finishesAtLabel")
-	if (!finishesAtLabel) {
+		const container = timeDisplay.cloneNode(false)
+		container.id = "endsAtContainer"
+		container.style.paddingLeft = 0
+
+		const separator = document.createElement("span")
+		separator.textContent = "•"
+		separator.classList.add("ytp-chapter-title-prefix")
+		container.appendChild(separator)
+
 		const label = document.createElement("span")
+		label.id = "endsAtLabel"
 		label.classList.add("ytp-time-duration")
-		label.id = "finishesAtLabel"
-		timeDisplay.appendChild(label)
+		container.appendChild(label)
+
+		timeDisplay.parentNode.insertBefore(container, timeDisplay.nextSibling)
 		return
 	}
 
+	const endsAtLabel = container.querySelector("#endsAtLabel")
+	if (!endsAtLabel) return
+
 	const { duration, playbackRate, currentTime } = player,
 		remaining = (duration - currentTime) / playbackRate,
-		finishesAt = new Date(Date.now() + remaining * 1000),
-		formatter = new Intl.DateTimeFormat([], { timeStyle: "short", hour12: false })
+		endsAt = new Date(Date.now() + remaining * 1000),
+		formatter = new Intl.DateTimeFormat([], {
+			timeStyle: "short",
+			hour12: false,
+		})
 
-	finishesAtLabel.textContent = ` - Ends at ${formatter.format(finishesAt)}`
+	endsAtLabel.textContent = `Ends at ${formatter.format(endsAt)}`
 }, 1000)
